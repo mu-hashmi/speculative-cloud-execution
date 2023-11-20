0472a79ba76f387170e60b2403a1c2462f29c8f3
Muhammad Hashmi <mhashmi@berkeley.edu>
2023-11-20 10:27:04 -0800

    Updated coordinator.py

--- coordinator.py
+++ coordinator.py
@@ -16,6 +16,19 @@ OutputT = TypeVar("OutputT")
 
 class SpeculativeOperator(abc.ABC, Generic[InputT, OutputT]):
     """Uses speculative execution to run models in the cloud and locally as a fallback."""
-    def use_cloud(self, rpc_handle,  msg_handler: Callable[[InputT, Timestamp], Optional[Tuple[OutputT, int]]]):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def execute_local(self, input_message: InputT) -> OutputT:
+        pass
+
+    def process_message(self, input_message: InputT) -> OutputT:
+        # needs to call execute_local after calling all the message handlers
+        pass
+
+    # commit comment
+    def use_cloud(self, rpc_handle,  msg_handler: Callable[[InputT, Timestamp], Optional[Tuple[OutputT, Deadline]]], priority: int):
+        # store rpc_handle, msg_handler, priority inside a data structure
         raise NotImplementedError()
     
