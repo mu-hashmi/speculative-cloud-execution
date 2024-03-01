8a1d54be316b01e34b29acb89e3d3034d383b59b
Muhammad Hashmi <mhashmi@berkeley.edu>
2024-03-01 14:10:07 -0800

    Added StreamingImageRpcHandle

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import time
 from typing import Union
 from collections.abc import Iterator
+import queue
 
 import grpc
 
@@ -35,11 +36,42 @@ class ImageRpcHandle(coordinator.RpcHandle[image_pb2.Request, image_pb2.Response
             return self.stub().ProcessImageSync(rpc_request)
 
 
+class StreamingIterator:
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.active = True
+    
+    def add_request(self, request):
+        self.queue.put(request)
+
+    def __iter__(self):
+        while self.active:
+            yield self.queue.get(block=True)
+
+class StreamingImageRpcHandle(coordinator.RpcHandle[image_pb2.Request, image_pb2.Response, image_pb2_grpc.GRPCImageStub]):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request_iterator = StreamingIterator()
+        stub = self.stub()
+        self.response_iterator = stub.ProcessImageStreaming(self.request_iterator)
+
+    def stub(self) -> image_pb2_grpc.GRPCImageStub:
+        return image_pb2_grpc.GRPCImageStub(self.channel)
+    
+    def __call__(self, rpc_request):
+        # Potential bug: this function might hang. May need to update some gRPC settings,
+        # e.g. set ("grpc.http2.write_buffer_size", 1) in the gRPC client options.
+        # Potential bug: the response might not correspond to the request. Check this!
+        self.streaming_iterator.add_request(rpc_request)
+        response = next(self.response_iterator)
+        return response
+
 def test_speculative_operator():
     # Create operator.
     operator = MyOperator()
     rpc_handle = ImageRpcHandle()
-    images = ['https://i.imgur.com/2lnWoly.jpg', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2023-08/230802-Waymo-driverless-taxi-ew-233p-e47145.jpg',
+    images = ['https://i.imgur.com/2lnWoly.jpg', 
+              'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2023-08/230802-Waymo-driverless-taxi-ew-233p-e47145.jpg',
               'https://farm8.staticflickr.com/7117/7624759864_f1940fbfd3_z.jpg',
               'https://farm8.staticflickr.com/7419/10039650654_5d5a8b6706_z.jpg',
               'https://farm8.staticflickr.com/7135/8156447421_191b777e05_z.jpg']
